7d5e28cc1f34877884767a64f9319155d244cfb3
sion <noismik97@gmail.com>
2024-08-23 14:06:43 +0900

    feat: 버튼 좌우 아이콘

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,24 +4,22 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '../../lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:bg-[#F4F4F5] disabled:text-label-small disabled:text-[#A1A1AA]',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        primary: 'bg-brand-primary',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        default: '',
+        primary:
+          'bg-brand-primary hover:bg-brand-primary/90 text-paragraph-small text-content-primary px-3 py-2',
+        secondary:
+          'text-paragraph-small bg-secondary hover:bg-secondary/90 text-core-white px-3 py-2',
+        outline: 'text-content-primary text-label-small border border-border-primary px-3 py-2',
+        light: 'bg-gray-500 hover:bg-gray-500/90 text-label-small text-gray-50 px-3 py-2',
+        ghost: 'text-label-small text-core-white px-3 py-2',
       },
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
     },
   }
 );
@@ -30,15 +28,24 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
-  startAdorment?: React.ReactElement;
-  endAdorment?: React.ReactElement;
+  /** 좌측 아이콘 */
+  startAdornment?: React.ReactElement;
+  /** 우측 아이콘 */
+  endAdornment?: React.ReactElement;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, children, variant, startAdornment, endAdornment, asChild = false, ...props },
+    ref
+  ) => {
     const Comp = asChild ? Slot : 'button';
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp className={cn(buttonVariants({ variant, className }))} ref={ref} {...props}>
+        {startAdornment && <span>{startAdornment}</span>}
+        {children}
+        {endAdornment && <span>{endAdornment}</span>}
+      </Comp>
     );
   }
 );
