586a62787f8c50a0ca8a2a0c15d1356b8dffd5ac
sion <noismik97@gmail.com>
2024-09-01 20:51:28 +0900

    feat: 임시 로그인페이지

--- src/pages/auth/sign_in/SignInPage.tsx
+++ src/pages/auth/sign_in/SignInPage.tsx
@@ -1,5 +1,19 @@
+import { KaKaoButton } from '../components';
+
 const SignInPage = () => {
-  return <div>Login</div>;
+  const handleClick = () => {
+    console.log('first');
+  };
+
+  return (
+    <main className="flex size-full items-center justify-center">
+      {/** 타이틀 */}
+      <div className="flex flex-col gap-12">
+        <h2 className="text-center text-heading-large">로그인</h2>
+        <KaKaoButton onClick={handleClick}>카카오톡으로 시작하기</KaKaoButton>
+      </div>
+    </main>
+  );
 };
 
 export default SignInPage;
--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 
 import ComponentUiTest from './pages/component-ui-test';
 import { SignupTypePage, SignupPage, SignupFormPage } from './pages/auth/sign_up';
+import SignInPage from './pages/auth/sign_in/SignInPage';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/test" element={<ComponentUiTest />} />
+
+      {/** 로그인 페이지 */}
+      <Route path="/signin" element={<SignInPage />} />
+
+      {/** 회원가입 페이지 */}
       <Route path="/signup-type" element={<SignupTypePage />} />
       <Route path="/signup/:type" element={<SignupPage />} />
       <Route path="/signup/:type/info" element={<SignupFormPage />} />
