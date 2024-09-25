51809880dec78820f74ff947850e4d9f9968fbb2
sion <noismik97@gmail.com>
2024-09-25 18:12:11 +0900

    feat: 카테고리 열림 상태 작업

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
@@ -7,33 +8,52 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './
 
 type Menu = { title: string; to: string; icon: JSX.Element };
 
-type TabList = Record<string, Menu[]>;
+type TabList = Record<
+  string,
+  {
+    categoryName: string;
+    menuList: Menu[];
+  }
+>;
 
 const tabList: TabList = {
-  dashboard: [{ title: '내 콘텐츠', to: PATH.content_list, icon: <StickyNote size={16} /> }],
-  instructor: [
-    { title: '강사 목록', to: PATH.instructor_list, icon: <UsersRound size={16} /> },
-    { title: '가입 요청', to: PATH.instructor_request, icon: <TicketCheck size={16} /> },
-  ],
-  member: [
-    { title: '회원 목록', to: PATH.member_list, icon: <UsersRound size={16} /> },
-    { title: '가입 요청', to: PATH.member_request, icon: <TicketCheck size={16} /> },
-  ],
+  content: {
+    categoryName: '콘텐츠 관리',
+    menuList: [{ title: '내 콘텐츠', to: PATH.content_list, icon: <StickyNote size={16} /> }],
+  },
+  instructor: {
+    categoryName: '강사 관리',
+    menuList: [
+      { title: '강사 목록', to: PATH.instructor_list, icon: <UsersRound size={16} /> },
+      { title: '가입 요청', to: PATH.instructor_request, icon: <TicketCheck size={16} /> },
+    ],
+  },
+  member: {
+    categoryName: '회원 관리',
+    menuList: [
+      { title: '회원 목록', to: PATH.member_list, icon: <UsersRound size={16} /> },
+      { title: '가입 요청', to: PATH.member_request, icon: <TicketCheck size={16} /> },
+    ],
+  },
 };
 
 const Sidebar = () => {
   const { pathname } = useLocation();
 
+  const [openedCategory] = useState(() => pathname.split('/')[1]);
+
   return (
     <div className="flex size-full">
-      {/** 메뉴 */}
       <div className="flex h-full w-[240px] flex-col gap-10 overflow-y-auto border-r-[1px] px-5 py-10">
-        <Accordion type="multiple" defaultValue={[]}>
-          {Object.entries(tabList).map(([category, menuList]) => (
-            <AccordionItem key={category} value={category}>
-              <AccordionTrigger className="text-label-small text-content-primary">{category}</AccordionTrigger>
-              {menuList.map((menu) => (
-                <AccordionContent key={category + menu.to}>
+        <Accordion type="multiple" defaultValue={[openedCategory]}>
+          {Object.entries(tabList).map(([domain, tab]) => (
+            <AccordionItem key={domain} value={domain}>
+              {/** 카테고리 */}
+              <AccordionTrigger className="text-label-small text-content-primary">{tab.categoryName}</AccordionTrigger>
+
+              {/** 메뉴 리스트 */}
+              {tab.menuList.map((menu) => (
+                <AccordionContent key={domain + menu.to}>
                   <Link to={menu.to}>
                     <Button
                       className={`w-full justify-start gap-2 text-label-small text-content-primary transition-all hover:bg-secondary-100 ${pathname !== menu.to ? 'text-content-secondary' : ''}`}
