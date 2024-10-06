8fe57a2aa6818e21b1ab58479f09320fdb1bac47
sion <noismik97@gmail.com>
2024-10-06 20:37:32 +0900

    feat: 사이드바 햄버거

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { memo, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
-import { StickyNote, TicketCheck, UsersRound } from 'lucide-react';
+import { AlignJustify, StickyNote, TicketCheck, UsersRound } from 'lucide-react';
 import { PATH } from '@/constant/urls';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 
 type Menu = { title: string; to: string; icon: JSX.Element };
 
@@ -42,33 +43,51 @@ const Sidebar = () => {
 
   const [openedCategory] = useState(() => pathname.split('/')[1]);
 
+  const Menu = memo(() => {
+    return (
+      <Accordion type="multiple" defaultValue={[openedCategory]}>
+        {Object.entries(tabList).map(([domain, tab]) => (
+          <AccordionItem key={domain} value={domain}>
+            {/** 카테고리 */}
+            <AccordionTrigger className="text-label-small text-content-primary">{tab.categoryName}</AccordionTrigger>
+
+            {/** 메뉴 리스트 */}
+            {tab.menuList.map((menu) => (
+              <AccordionContent key={domain + menu.to}>
+                <Link to={menu.to}>
+                  <Button
+                    className={`w-full justify-start gap-2 text-label-small text-content-primary transition-all hover:bg-secondary-100 ${pathname !== menu.to ? 'text-content-secondary' : ''}`}
+                    startAdornment={menu.icon}
+                    variant="custom"
+                  >
+                    {menu.title}
+                  </Button>
+                </Link>
+              </AccordionContent>
+            ))}
+          </AccordionItem>
+        ))}
+      </Accordion>
+    );
+  });
+
   return (
     <div className="flex size-full">
-      <div className="flex h-full w-[240px] flex-col gap-10 overflow-y-auto border-r-[1px] px-5 py-10">
-        <Accordion type="multiple" defaultValue={[openedCategory]}>
-          {Object.entries(tabList).map(([domain, tab]) => (
-            <AccordionItem key={domain} value={domain}>
-              {/** 카테고리 */}
-              <AccordionTrigger className="text-label-small text-content-primary">{tab.categoryName}</AccordionTrigger>
-
-              {/** 메뉴 리스트 */}
-              {tab.menuList.map((menu) => (
-                <AccordionContent key={domain + menu.to}>
-                  <Link to={menu.to}>
-                    <Button
-                      className={`w-full justify-start gap-2 text-label-small text-content-primary transition-all hover:bg-secondary-100 ${pathname !== menu.to ? 'text-content-secondary' : ''}`}
-                      startAdornment={menu.icon}
-                      variant="custom"
-                    >
-                      {menu.title}
-                    </Button>
-                  </Link>
-                </AccordionContent>
-              ))}
-            </AccordionItem>
-          ))}
-        </Accordion>
+      {/** 사이드 바 */}
+      <div className="mobile:w-fit flex h-full w-[240px] flex-col overflow-y-auto border-r-[1px] px-5 py-10">
+        <Sheet>
+          <SheetTrigger className="mobile:block hidden">
+            <AlignJustify />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <Menu />
+          </SheetContent>
+        </Sheet>
+        <div className="mobile:hidden">
+          <Menu />
+        </div>
       </div>
+
       <div className="w-full overflow-y-auto">
         <Outlet />
       </div>
